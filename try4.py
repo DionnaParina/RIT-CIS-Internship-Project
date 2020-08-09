cd1f755c7da035c9836c7fe8e23b221107d02427
Dionna Parina <68387555+DionnaParina@users.noreply.github.com>
2020-08-09 17:27:33 -0400

    Add files via upload

--- try4.py
+++ try4.py
@@ -9,6 +9,7 @@ Created on Tue Jul 21 09:54:37 2020
 import cv2
 import numpy as np
 import time
+from matplotlib import pyplot as plt
 cap = cv2.VideoCapture('C:/Users/diada/Downloads/blink.avi')
 #fourcc = cv2.VideoWriter_fourcc(*'XVID')
 #out = cv2.VideoWriter('C:/Users/diada/Downloads/Copy of 1_blink.avi', fourcc, 80.0, (640,480))
@@ -35,19 +36,39 @@ for i in range(1,len(frames)-1):
     gaus = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
     ret,otsu = cv2.threshold(gray, 125, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     laplacian = cv2.Laplacian(gray, cv2.CV_64F)
-    edges = cv2.Canny(gray, 50, 10)
+    edges = cv2.Canny(gray, 1, 50)
+    kernel = np.ones((5,5), np.uint8)
+    erosion = cv2.erode(gray, kernel, iterations =1)
+    dilation= cv2.dilate(edges, kernel, iterations =1)
+    gausblur = cv2.GaussianBlur(gray, (5,5), cv2.BORDER_DEFAULT)
     
-    cv2.imshow('thresh1', thresh1)
-    cv2.imshow('gaus', gaus)
-    cv2.imshow('otsu', otsu)
-    cv2.imshow('laplacian', laplacian)
+    
+     #cv2.setMouseCallback('dilation', dilation)
+   # def mousecallback(event, x,y, flags, param):
+        #for i in range (0, contours.size):
+         #   r=cv2.pointPolygonTest(contours[i],  (y,x), False)
+           # if r>0:
+               # print ("selected contour" +i)
+    _, contours, _= cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    image = cv2.drawContours(dilation.copy(), contours, -1, (0,255, 0), 2)
+    print(contours) 
+    #cv2.imshow('thresh1', thresh1)
+    #cv2.imshow('gaus', gaus)
+    #cv2.imshow('otsu', otsu)
+    #cv2.imshow('laplacian', laplacian)
+    #cv2.imshow('gaussian blur', gausblur)
+    #cv2.imshow('erosion', erosion)
+    #cv2.imshow('dilation', dilation)
     cv2.imshow('edges', edges)
     cv2.imshow('gray', gray)
+    cv2.imshow('dilation.copy()', dilation.copy())
+    plt.imshow(image)
     
     time.sleep(.25)
     if cv2.waitKey(1) & 0xFF ==ord('q'):
         break
 cv2.waitKey(0)
+
 
 
 
